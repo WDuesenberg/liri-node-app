31d203c2c6e291895655dce04f922540c339877e
w_duse89 <wyatt.duesenberg@gmail.com>
2019-10-03 14:45:08 -0600

    update

--- liri.js
+++ liri.js
@@ -1,52 +1,63 @@
 require("dotenv").config();
+var request = require("request");
+var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
-var inquirer = require("inquirer");
 var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
+var inquirer = require("inquirer");
+
+var userOption = process.argv[2];
+var inputParameter = process.argv[3];
+console.log(process.argv)
 
 
-console.log("init");
-console.log("concertThis");
+// console.log("init");
+// console.log("concertThis");
 
 function init() {
     inquirer.prompt([
         {
-            type:"list",
-            name:"userChoice",
-            message:"What do yo want to do?",
-            choices:["concert-this", "spotify-this-song", "movie-this", "do-what-it-says", "quit"] 
+            type: "list",
+            name: "userChoice",
+            message: "What do yo want to do?",
+            choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says", "quit"]
         }
-    ]).then(function (answer){
+    ]).then(function (answer) {
         console.log(answer)
-    });
+    })
 }
-        
 
-function concertThis(artist) {
-    url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp",
-    axios.get("url").then(
-        function(response) {
-            console.log("Name of the venue:" + response.data.Venue);
-        }
-    ); 
-    init();            
-}
-    
-    
-function spotifyThisSong() {
-    var spotify = new Spotify(keys.spotify);
 
-    console.log("spotifyThisSong");
-    init();    
-}
-function movieThis() {
-    console.log("movieThis");
-    init();
-}
-function doWhatItSays() {
-    console.log("doWhatItSays");
-    init();
+// function concertThis(artist) {
+//     url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp",
+//         axios.get("url").then(
+//             function (response) {
+//                 console.log("Name of the venue: " + response.data.venue);
 
-}
-init();
-// http://www.omdbapi.com/?i=tt3896198&apikey=c95cf74e
+//                 init();
+
+//                 function spotifyThisSong() {
+//                     console.log("spotifyThisSong");
+
+//                     init();
+//                 }
+//                 function movieThis() {
+//                     console.log("movieThis");
+//                     init();
+//                 }
+//                 function doWhatItSays() {
+//                     console.log("doWhatItSays");
+//                     init();
+
+//                 }
+//             }
+//         )}
+
+
+                init();
+
+
+
+
+
