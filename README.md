62064cbbe11e26cfc96e8fec49e36f360bc0a991
w_duse89 <wyatt.duesenberg@gmail.com>
2019-10-03 22:39:24 -0600

    update

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
 # liri-node-app
+LIRI is a Language Interpretation and Recognition Interface.
+
+# Overview
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI is a command line node app that takes in parameters and gives data in return.
+
+# Overall Use
+Use Node JS to create a LIRI bot, like iPhone's SIRI, but takes in commands through Language vs Speech using the following commands:
+
+concert-this
+
+spotify-this-song
+
+movie-this
+
+do-what-it-says
+
+# Technology Used
+Github - file repo
+VScode - code/text editor
+Node.js
+
+API(s):
+Spotify
+OMBD
+Bands In Town
+
+NPM packages:
+Node-spotify-api
+Axios
+Moment
+Dotenv
+
+Required for use:
+Node.js
+Make new Github repo called liri-node-app and clone to user your computer.
+Send requests using the axios package to the Bands in Town, Spotify and OMBD API's.
+
+# What Each Command Does
+LIRI searches Bands in Town for concerts, Spotify for songs, and OMBD for movies.
+    
+    1. node liri.js concert-this '<artist/band name here>'
+       
+       Searches the Bands in Town Artist Events API for an artist and renders the following information about each event to the terminal.
+
+            Name of the venue.
+
+            Venue location.
+
+            Date of the Event (using the format "MM/DD/YYYY").
+   
+    2. node liri.js spotify-this-song '<song name here>'
+        
+        Displays the following information about the selected song in your terminal/bash window.
+
+            Artist(s)
+
+            The song's name.
+
+            A preview link of the song from Spotify.
+
+            The album containing the song.
+
+        If no song is provided, the program defaults to "The Sign" by Ace of Base.
+
+    3. node liri.js movie-this '<movie name here>'
+
+        This will output the following movie information to your terminal/bash window:
+
+            * Title
+            * Year movie was released
+            * IMDB Rating
+            * Rotten Tomatoes Rating
+            * Country where movie was produced
+            * Language(s)
+            * Plot of the movie
+            * Actors in the movie
+        
+        If the user does not enter a movie selection, the program outputs data for the movie 'Mr. Nobody.'
+
+    4. node liri.js do-what-it-says
+
+        Runs spotify-this-song for "I Want it That Way".
+
+
+
+
+ 
+
+
--- liri.js
+++ liri.js
@@ -1,61 +1,154 @@
 require("dotenv").config();
-var request = require("request");
-var fs = require("fs");
+// Add the code required to import the .keys.js file and store it in a variable.
+var keys = require("./keys.js");
 var axios = require("axios");
 var moment = require("moment");
-var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret,
+});
+// var defaultSong = require("The Sign");
+var defaultMovie = "Mr Nobody";
 // var spotify = new Spotify(keys.spotify);
-var inquirer = require("inquirer");
-
-var userOption = process.argv[2];
-var inputParameter = process.argv[3];
-console.log(process.argv)
 
 
-// console.log("init");
-// console.log("concertThis");
+/**
+ * Name of the venue
+ * Venue location
+ * Date of the Event (use moment to format this as "MM/DD/YYYY")
+ */
+var action = process.argv[2];
+var value = process.argv[3];
+// console.log(process.argv)
 
-function init() {
-    inquirer.prompt([
-        {
-            type: "list",
-            name: "userChoice",
-            message: "What do yo want to do?",
-            choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says", "quit"]
+switch (action) {
+    case "concert-this":
+        getBands(value)
+        break;
+    case "spotify-this-song":
+        // If user has not specified a song, use default
+        // if (value === "") {
+        //  value = defaultSong;    
+        //    }
+        getSongs(value)
+        break;
+    case "movie-this":
+        // If user has not specified a movie, use default
+        if (value == "") {
+            value = defaultMovie;
         }
-    ]).then(function (answer) {
-        console.log(answer)
-    })
+        getMovies(value)
+        break;
+    case "do-what-it-says":
+        doWhatItSays()
+        break;
+        default:
+        break;
 }
+function getBands(artist) {
+    // var artist = value;
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            console.log("Name of the venue: ", response.data[0].venue.name);
+            console.log("Venue location: ", response.data[0].venue.location);
+            var eventDate = moment(response.data[0].datetime).format("MM/DD/YYYY");
+            console.log("Date of the Event:", eventDate);
+        })    
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+function getSongs(songName) {
+    // var songName = value;
+
+    // If user has not specified a song, default to "The Sign" by Ace of Bass
+    if (songName === "") {
+        songName = "I saw the Sign";
+    }
+
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        // else {
+        //      console.log("Not right now. Later?")
+        //      console.log(JSON.stringify(data));
+        //      The song's name
+        //Artist(s)
+        console.log("Artists: ", data.tracks.items[0].album.artists[0].name)
+        // A preview link of the song from Spotify
+        console.log("Preview Link: ", data.tracks.items[0].preview_url)
+        // The album that the song is from
+        console.log("Album Name: ", data.tracks.items[0].album.name)    
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    }
+
+function getMovies(movieName) {
+    // var movieName = value;
+    axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=c95cf74e=" + movieName)
+        .then(function (data) {
+            // console.log(data.data);
+            var results = `
+            Title of the movie: ${data.data.Title}
+            Year the movie came out: ${data.data.Year}
+            IMBD Rating of the movie: ${data.data.Rated}
+            Rotten Tomatoes Rating of the movie: ${data.data.Ratings[1].Value}
+            Countrywhere the movie was produced: ${data.data.Country}
+            Language of the movie: ${data.data.Language}
+            Plot of the movie: ${data.data.Plot}
+            Actors in the movie: ${data.data.Actors}`;
+            console.log(results)
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+        // Response if the user does not type in a movie title
+        if (movieName === "Mr. Nobody") {
+            console.log("---------------");
+            console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
+        };
+    }
+
+    function doWhatItSays() {
+        fs.readFile("random.txt", "utf8", function (err, data) {
+            data = data.split(",");
+            var action = data[0]
+            var value = data[1]
+            // getSongs(value)
+            switch (action) {
+                case "concert-this":
+                    getBands(value)
+                    break;
+                case "spotify-this-song":
+                    getSongs(value)
+                    break;
+                case "movie-this":
+                    getMovies(value)
+                    break;
+                default:
+                    break;
+                }
+            });
+        }
+// imports.spotify
+// Issues with certain property(s) not being defined.
+// appears as though packages are loaded but havent been able to trouble shoot the issues :(
+ 
+            
 
 
-// function concertThis(artist) {
-//     url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp",
-//         axios.get("url").then(
-//             function (response) {
-//                 console.log("Name of the venue: " + response.data.venue);
-
-//                 init();
-
-//                 function spotifyThisSong() {
-//                     console.log("spotifyThisSong");
-
-//                     init();
-//                 }
-//                 function movieThis() {
-//                     console.log("movieThis");
-//                     init();
-//                 }
-//                 function doWhatItSays() {
-//                     console.log("doWhatItSays");
-//                     init();
-
-//                 }
-//             }
-//         )}
 
 
-                init();
+
+               
 
 
 
